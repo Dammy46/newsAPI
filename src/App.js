078fde0b565534ddf4d5f1c113e0f74f1cb35e92
Dev Dammy <cranedammy46@gmail.com>
2022-04-23 21:09:45 +0100

    build app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import Articles from './Articles';
-import axios from 'axios'
+import axios from 'axios';
 import ErrorBoundary from './ErrorBoundary';
 import './App.css';
 
 const App = () => {
-  const [data, setDate] = useState([])
-  const key = "9eff43c9224547c895fbb96179dd4458"
+  const [data, setDate] = useState([]);
+  const key = '9eff43c9224547c895fbb96179dd4458';
 
   useEffect(() => {
-    axios.get(
-      `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${key}`
-    ).then((res) => setDate(res.data.articles))
-    .then((error)=> console.log(error))
-}, [data]);
-  return !data.length ?
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${key}`
+      )
+      .then((res) => setDate(res.data.articles))
+      .then((error) => console.log(error));
+  }, [data]);
+  return !data.length ? (
     <h1>Loading....</h1>
-    :
-    (
-      <div className="mw-7 center">
-        <h2>📰NEWS</h2>
-        <ErrorBoundary>
-          <Articles news={(data)} />
-        </ErrorBoundary>
-      </div>
-    );
-   
-}
+  ) : (
+    <div className="mw-7 center">
+      <h2>📰NEWS FEED</h2>
+      <ErrorBoundary>
+        <Articles news={data} />
+      </ErrorBoundary>
+    </div>
+  );
+};
 
 export default App;
